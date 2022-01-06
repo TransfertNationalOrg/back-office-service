e0451658e3289cdca3ee3d01c3829a6588939c00
ambou-97 <ambroise.tine@gmail.com>
2022-01-06 13:39:32 +0100

    Departure point

---
+++ src/main/java/ma/ensa/BackOfficeServiceApplication.java
@@ -0,0 +1,13 @@
+package ma.ensa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackOfficeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackOfficeServiceApplication.class, args);
+	}
+
+}
