c2dcee8a1d62fe83c06e4a24a628b5f2e0ed8e13
ambou-97 <ambroise.tine@gmail.com>
2022-01-06 17:50:27 +0100

    Repositories

---
+++ src/main/java/ma/ensa/repository/AgentRepository.java
@@ -0,0 +1,7 @@
+package ma.ensa.repository;
+
+import ma.ensa.model.Agent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgentRepository extends JpaRepository<Agent, Long> {
+}
---
+++ src/main/java/ma/ensa/repository/ParametreRepository.java
@@ -0,0 +1,7 @@
+package ma.ensa.repository;
+
+import ma.ensa.model.Parametre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParametreRepository extends JpaRepository<Parametre, Long> {
+}
