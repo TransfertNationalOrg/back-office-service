887dcd74be8b24b8b6ddaabfbcf070b880f84a33
ambou-97 <ambroise.tine@gmail.com>
2022-01-06 17:58:11 +0100

    controllers

---
+++ src/main/java/ma/ensa/controller/AgentController.java
@@ -0,0 +1,51 @@
+package ma.ensa.controller;
+
+import lombok.Data;
+import ma.ensa.converter.AgentConverter;
+import ma.ensa.dto.AgentDTO;
+import ma.ensa.service.AgentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("agent")
+@Data
+public class AgentController {
+
+    final AgentService agentService;
+    final AgentConverter agentConverter;
+
+    @PostMapping("/")
+    public ResponseEntity<?> save(@Valid @RequestBody AgentDTO agentDTO) throws Exception {
+        if (agentDTO == null)
+            return ResponseEntity.badRequest().body("The provided agent is not valid");
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(agentConverter.convertToDTO(agentService.save(agentConverter.convertToDM(agentDTO))));
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<?> update(@Valid @RequestBody AgentDTO agentDTO) throws Exception {
+        if (agentDTO == null)
+            return ResponseEntity.badRequest().body("The provided agent is not valid");
+        return ResponseEntity
+                .ok().body(agentConverter.convertToDTO(agentService.update(agentConverter.convertToDM(agentDTO))));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) throws Exception {
+        if(id == null)
+            return ResponseEntity.badRequest().body("The provided agent's id is not valid");
+        return ResponseEntity.ok().body("Agent [" + agentService.delete(id) + "] deleted successfully.");
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<AgentDTO>> findAll() {
+        return ResponseEntity.ok().body(agentConverter.convertToDTOs(agentService.findAll()));
+    }
+
+}
---
+++ src/main/java/ma/ensa/controller/ParametreController.java
@@ -0,0 +1,50 @@
+package ma.ensa.controller;
+
+import lombok.Data;
+import ma.ensa.converter.ParametreConverter;
+import ma.ensa.dto.ParametreDTO;
+import ma.ensa.service.ParametreService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("parametre")
+@Data
+public class ParametreController {
+    
+    final ParametreService parametreService;
+    final ParametreConverter parametreConverter;
+
+    @PostMapping("/")
+    public ResponseEntity<?> save(@Valid @RequestBody ParametreDTO parametreDTO) throws Exception {
+        if (parametreDTO == null)
+            return ResponseEntity.badRequest().body("The provided parametre is not valid");
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(parametreConverter.convertToDTO(parametreService.save(parametreConverter.convertToDM(parametreDTO))));
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<?> update(@Valid @RequestBody ParametreDTO parametreDTO) throws Exception {
+        if (parametreDTO == null)
+            return ResponseEntity.badRequest().body("The provided parametre is not valid");
+        return ResponseEntity
+                .ok().body(parametreConverter.convertToDTO(parametreService.update(parametreConverter.convertToDM(parametreDTO))));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) throws Exception {
+        if(id == null)
+            return ResponseEntity.badRequest().body("The provided parametre's id is not valid");
+        return ResponseEntity.ok().body("Agent [" + parametreService.delete(id) + "] deleted successfully.");
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<ParametreDTO>> findAll() {
+        return ResponseEntity.ok().body(parametreConverter.convertToDTOs(parametreService.findAll()));
+    }
+}
