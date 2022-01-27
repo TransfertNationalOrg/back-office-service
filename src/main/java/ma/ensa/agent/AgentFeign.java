0f88c04e211cb2bbf5e18c215da3a680de3ae173
ambou-97 <ambroise.tine@gmail.com>
2022-01-27 22:57:30 +0100

    AgentFeign

--- src/main/java/ma/ensa/agent/AgentFeign.java
+++ src/main/java/ma/ensa/agent/AgentFeign.java
@@ -1,18 +1,41 @@
 package ma.ensa.agent;
 
+import ma.ensa.Transfert.TransfertDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name="transfert-service")
+@FeignClient(name="agent-service")
 public interface AgentFeign {
+    //AGENT-SERVICE
+
     //Get all agents from agent-service
-    @GetMapping("agent/")
+    @GetMapping("/agent/")
     List<AgentDTO> findAll();
 
-    //Get a agent by id from agent-service
-    @GetMapping("agent/{idAgent}")
+    //Get an agent by id from agent-service
+    @GetMapping("/agent/{idAgent}")
     AgentDTO getAgentById(@PathVariable("id") Long id);
+
+    //Add an agent
+    @PostMapping("/agent/")
+    ResponseEntity<?> save (@RequestBody AgentDTO agentDTO);
+
+    //Update an agent
+    @PutMapping("/agent/")
+    ResponseEntity<?> update(@RequestBody AgentDTO agentDTO);
+
+    //Delete an agent
+    @DeleteMapping("/agent/{id}")
+    ResponseEntity<?> delete(@PathVariable Long id);
+
+    //TRANSFERT-SERVICE
+
+    //Get all transferts by idAgent
+    @GetMapping("/transfert/agent/{id}")
+    List<TransfertDTO> getTransfertsByAgent(@PathVariable("id") Long id);
+
+
 }
