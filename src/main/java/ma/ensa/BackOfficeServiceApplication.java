5da1bd03f864a615c36c51fb918fda7f038441ab
ambou-97 <ambroise.tine@gmail.com>
2022-02-04 01:47:32 +0100

    CommandLineRunner

--- src/main/java/ma/ensa/BackOfficeServiceApplication.java
+++ src/main/java/ma/ensa/BackOfficeServiceApplication.java
@@ -1,15 +1,32 @@
 package ma.ensa;
 
+import lombok.Data;
+import ma.ensa.model.Parametre;
+import ma.ensa.repository.ParametreRepository;
+import ma.ensa.service.ParametreService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
+@Data
 @SpringBootApplication
 @EnableFeignClients
-public class BackOfficeServiceApplication {
+public class BackOfficeServiceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackOfficeServiceApplication.class, args);
 	}
 
+	private final ParametreService parametreService;
+	private final ParametreRepository parametreRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		if(parametreRepository.findAll().size()==0){
+			Parametre parametre = new Parametre();
+			parametre.setId(1L);
+			parametreService.save(parametre);
+		}
+
+	}
 }
